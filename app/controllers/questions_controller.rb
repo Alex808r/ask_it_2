d93dbbaf5f966c1c7a684073f9085adb04300546
Alex808r <fromxlam@gmail.com>
2022-09-29 21:12:00 +0300

    Lesson 4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
     # render plain: params
     @question = Question.new(question_params)
     if @question.save
+      flash[:success] = 'Question created!'
       redirect_to(questions_path)
     else
       render :new
@@ -29,6 +30,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find_by(id: params[:id])
     if @question.update(question_params)
+      flash[:success] = 'Question updated!'
       redirect_to(questions_path)
     else
       render :edit
@@ -38,6 +40,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find_by(id: params[:id])
     @question.destroy
+    flash[:success] = 'Question destroy!'
     redirect_to(questions_path)
   end
 
