14e53b7ad587a51da395bd063f29e384810ef204
Alex808r <78639180+Alex808r@users.noreply.github.com>
2022-10-01 15:10:30 +0300

    Lesson 7 (#7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+  title = Faker::Hipster.sentence(word_count: 3)
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4) #=> "Vomito unde uxor annus. Et patior utilis sursum."
+  Question.create(title: title, body: body)
+end
