a546de667476cc3518d1d568b9830610944aef2a
Alex808r <fromxlam@gmail.com>
2022-10-04 19:35:41 +0300

    Lesson 10

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -6,7 +6,41 @@ module Authentication
     private
 
     def current_user
-      @current_user ||= User.find(session[:user_id]).decorate if session[:user_id].present?
+      user = session[:user_id].present? ? user_from_session : user_from_token
+
+      @current_user ||= user&.decorate
+    end
+
+    def user_from_session
+      User.find_by(id: session[:user_id])
+    end
+
+    def user_from_token
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      token = cookies.encrypted[:remember_token]
+
+      return unless user&.remember_token_authenticated?(token)
+
+      sign_in user
+      user
+    end
+
+    # def current_user
+    #   if session[:user_id].present?
+    #   @current_user ||= User.find(session[:user_id]).decorate
+    #   elsif cookies.encrypted[:user_id].present?
+    #     user = User.find(cookies.encrypted[:user_id])
+    #     if user&.remember_token_authenticated?(cookies.encrypted[:remember_token])
+    #       sign_in(user)
+    #       @current_user ||= user.decorate
+    #     end
+    #   end
+    # end
+
+    def forget(user)
+      user.forget_me
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
     end
 
     def user_signed_in?
@@ -18,6 +52,7 @@ module Authentication
     end
 
     def sign_out
+      forget(current_user)
       session.delete(:user_id)
       @current_user = nil
     end
@@ -32,6 +67,12 @@ module Authentication
       return if user_signed_in?
       flash[:warning] = 'You are not Signed In'
       redirect_to root_path
+    end
+
+    def remember(user)
+      user.remember_me
+      cookies.encrypted.permanent[:remember_token] = user.remember_token
+      cookies.encrypted.permanent[:user_id] = user.id
     end
 
     helper_method :current_user, :user_signed_in?
